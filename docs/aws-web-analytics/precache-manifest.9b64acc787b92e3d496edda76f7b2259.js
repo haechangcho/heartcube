50510d9cada21b1c2e9e59d909383c5fde644be4
Pavel Tiunov <pavel.tiunov@gmail.com>
2018-12-20 13:53:01 +0300

    Push aws-web-analytics dashboard to github

--- docs/aws-web-analytics/precache-manifest.9b64acc787b92e3d496edda76f7b2259.js
+++ docs/aws-web-analytics/precache-manifest.9b64acc787b92e3d496edda76f7b2259.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.4a686d48.js"
   },
   {
-    "revision": "e077b46078fe911d483e",
-    "url": "./static/js/main.e077b460.chunk.js"
+    "revision": "ce7c83a67c2c8f6b8410",
+    "url": "./static/js/main.ce7c83a6.chunk.js"
   },
   {
     "revision": "4503ac75343e659451a6",
     "url": "./static/js/1.4503ac75.chunk.js"
   },
   {
-    "revision": "e077b46078fe911d483e",
+    "revision": "ce7c83a67c2c8f6b8410",
     "url": "./static/css/main.913ecbaa.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "./static/css/1.79fd6dd0.chunk.css"
   },
   {
-    "revision": "ccb45f479a8faa026230b38a7f75a468",
+    "revision": "a1a1ba9a27cc45485559a9f85bac2fa2",
     "url": "./index.html"
   }
 ];
