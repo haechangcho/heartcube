haechangcho/heartcube

commit 6a85943eab812d8eeac9b50558500d040b1d36fc

feat: GraphQL - exponse annotation/lastRefreshTime in extensions (#10428)

packages/cubejs-api-gateway/src/graphql.ts
@@ -646,7 +646,7 @@ export function makeSchema(metaConfig: any): GraphQLSchema {
             type: 'RootOrderByInput'
           }),
         },
-        resolve: async (_, args, { req, apiGateway }, info) => {
+        resolve: async (_, args, { req, res, apiGateway }, info) => {
           const query = getJsonQuery(metaConfig, args, info);
 
           const results = await new Promise<any>((resolve, reject) => {
@@ -674,15 +674,20 @@ export function makeSchema(metaConfig: any): GraphQLSchema {
           // TODO: Move postprocessing to native?
           parseDates(results);
 
+          res.extensions = {
+            annotation: results.annotation,
+            lastRefreshTime: results.lastRefreshTime,
+          };
+
           return results.data.map(entry => R.toPairs(entry)
-            .reduce((res, pair) => {
+            .reduce((accum, pair) => {
               let path = pair[0].split('.');
               path[0] = unCapitalize(path[0]);
               if (results.annotation.dimensions[pair[0]]?.type === 'time') {
                 path = [...path, 'value'];
               }
               return (results.annotation.timeDimensions[pair[0]] && path.length !== 3)
-                ? res : R.set(R.lensPath(path), pair[1], res);
+                ? accum : R.set(R.lensPath(path), pair[1], res);
             }, {}));
         }
       });

packages/cubejs-api-gateway/test/__snapshots__/graphql.test.ts.snap
@@ -1,5 +1,41 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
+exports[`GraphQL Schema extensions should return annotation and lastRefreshTime in extensions 1`] = `
+Object {
+  "data": Object {
+    "cube": Array [
+      Object {
+        "orders": Object {
+          "count": null,
+          "status": "completed",
+        },
+      },
+    ],
+  },
+  "extensions": Object {
+    "annotation": Object {
+      "dimensions": Object {
+        "Orders.status": Object {
+          "shortTitle": "Status",
+          "title": "Orders Status",
+          "type": "string",
+        },
+      },
+      "measures": Object {
+        "Orders.count": Object {
+          "shortTitle": "Count",
+          "title": "Orders Count",
+          "type": "number",
+        },
+      },
+      "segments": Object {},
+      "timeDimensions": Object {},
+    },
+    "lastRefreshTime": "2025-06-01T12:00:00.000Z",
+  },
+}
+`;
+
 exports[`GraphQL Schema with camelCase GraphQL query 0: query CubeQuery {
   cube {
     orders {

packages/cubejs-api-gateway/test/graphql.test.ts
@@ -151,15 +151,16 @@ describe('GraphQL Schema', () => {
 
     app.use('/graphql', jsonParser, (req, res) => {
       const schema = makeSchema(metaConfig);
-  
+
       return graphqlHTTP({
         schema,
         context: {
           req,
+          res,
           apiGateway: {
             async load({ query, res: response }) {
               expect(query).toMatchSnapshot(req.body.query);
-  
+
               response({
                 query,
                 annotation: {},
@@ -172,18 +173,19 @@ describe('GraphQL Schema', () => {
     });
 
     const GRAPHQL_QUERIES_PATH = `${process.cwd()}/test/graphql-queries/base.gql`;
-    
+
     app.use('/graphql', jsonParser, (req, res) => {
       const schema = makeSchema(metaConfig);
-    
+
       return graphqlHTTP({
         schema,
         context: {
           req,
+          res,
           apiGateway: {
             async load({ query, res: response }) {
               expect(query).toMatchSnapshot(req.body.query);
-    
+
               response({
                 query,
                 annotation: {},
@@ -194,7 +196,7 @@ describe('GraphQL Schema', () => {
         },
       })(req, res);
     });
-    
+
     test('should make valid schema', () => {
       const schema = makeSchema(metaConfig);
       expectValidSchema(schema);
@@ -221,21 +223,22 @@ describe('GraphQL Schema', () => {
       });
     });
   });
-    
+
   describe('with snake_case', () => {
     const app = express();
 
     app.use('/graphql', jsonParser, (req, res) => {
       const schema = makeSchema(metaConfigSnakeCase);
-      
+
       return graphqlHTTP({
         schema,
         context: {
           req,
+          res,
           apiGateway: {
             async load({ query, res: response }) {
               expect(query).toMatchSnapshot(req.body.query);
-      
+
               response({
                 query,
                 annotation: {},
@@ -260,6 +263,75 @@ describe('GraphQL Schema', () => {
 
         expect((<any>error)?.text).toBeUndefined();
       });
+    });
+  });
+
+  describe('extensions', () => {
+    const mockAnnotation = {
+      measures: {
+        'Orders.count': {
+          title: 'Orders Count',
+          shortTitle: 'Count',
+          type: 'number',
+        },
+      },
+      dimensions: {
+        'Orders.status': {
+          title: 'Orders Status',
+          shortTitle: 'Status',
+          type: 'string',
+        },
+      },
+      segments: {},
+      timeDimensions: {},
+    };
+
+    const mockLastRefreshTime = '2025-06-01T12:00:00.000Z';
+
+    const app = express();
+
+    app.use('/graphql', jsonParser, (req: any, res: any) => {
+      const schema = makeSchema(metaConfig);
+
+      return graphqlHTTP({
+        schema,
+        context: {
+          req,
+          res,
+          apiGateway: {
+            async load({ query, res: response }) {
+              response({
+                query,
+                annotation: mockAnnotation,
+                lastRefreshTime: mockLastRefreshTime,
+                data: [
+                  { 'Orders.count': 10, 'Orders.status': 'completed' },
+                ],
+              });
+            },
+          },
+        },
+        extensions: () => res.extensions || {},
+      })(req, res);
+    });
+
+    test('should return annotation and lastRefreshTime in extensions', async () => {
+      const query = `query CubeQuery {
+        cube {
+          orders { count status }
+        }
+      }`;
+
+      const res = await request(app)
+        .post('/graphql')
+        .set('Content-Type', 'application/json')
+        .send(gqlQuery(query));
+
+      expect(res.body.errors).toBeUndefined();
+      expect(res.body.data.cube).toBeDefined();
+      expect(res.body.extensions).toBeDefined();
+
+      expect(res.body).toMatchSnapshot();
     });
   });
 });

packages/cubejs-api-gateway/src/gateway.ts
@@ -303,11 +303,13 @@ class ApiGateway {
           schema,
           context: {
             req,
+            res,
             apiGateway: this
           },
           graphiql: getEnv('nodeEnv') !== 'production'
             ? { headerEditorEnabled: true }
             : false,
+          extensions: () => (res as any).extensions || {},
         })(req, res);
       })
     );

commit 8392373e243624a07cb113a206662be5ca986741

fix(api-gateway): Returns null for all measures except the last one, fix #10482 (#10495)
It's a regression introduced in #10428
I've renamed res to accum to protect possible errors with shadow variables, and I miss 1 place :(

packages/cubejs-api-gateway/src/graphql.ts
@@ -683,11 +683,13 @@ export function makeSchema(metaConfig: any): GraphQLSchema {
             .reduce((accum, pair) => {
               let path = pair[0].split('.');
               path[0] = unCapitalize(path[0]);
+
               if (results.annotation.dimensions[pair[0]]?.type === 'time') {
                 path = [...path, 'value'];
               }
+
               return (results.annotation.timeDimensions[pair[0]] && path.length !== 3)
-                ? accum : R.set(R.lensPath(path), pair[1], res);
+                ? accum : R.set(R.lensPath(path), pair[1], accum);
             }, {}));
         }
       });

packages/cubejs-api-gateway/test/__snapshots__/graphql.test.ts.snap
@@ -6,8 +6,16 @@ Object {
     "cube": Array [
       Object {
         "orders": Object {
-          "count": null,
+          "count": "10",
           "status": "completed",
+          "totalAmount": "500",
+        },
+      },
+      Object {
+        "orders": Object {
+          "count": "5",
+          "status": "pending",
+          "totalAmount": "200",
         },
       },
     ],
@@ -25,6 +33,11 @@ Object {
         "Orders.count": Object {
           "shortTitle": "Count",
           "title": "Orders Count",
+          "type": "number",
+        },
+        "Orders.totalAmount": Object {
+          "shortTitle": "Total Amount",
+          "title": "Orders Total Amount",
           "type": "number",
         },
       },

packages/cubejs-api-gateway/test/graphql.test.ts
@@ -7,7 +7,6 @@ import { graphqlHTTP } from 'express-graphql';
 import { GraphQLObjectType } from 'graphql';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import fs from 'fs-extra';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import request from 'supertest';
 
 import { makeSchema } from '../src/graphql';
@@ -19,6 +18,10 @@ const metaConfig = [
       measures: [
         {
           name: 'Orders.count',
+          isVisible: true,
+        },
+        {
+          name: 'Orders.totalAmount',
           isVisible: true,
         },
       ],
@@ -274,6 +277,11 @@ describe('GraphQL Schema', () => {
           shortTitle: 'Count',
           type: 'number',
         },
+        'Orders.totalAmount': {
+          title: 'Orders Total Amount',
+          shortTitle: 'Total Amount',
+          type: 'number',
+        },
       },
       dimensions: {
         'Orders.status': {
@@ -305,7 +313,8 @@ describe('GraphQL Schema', () => {
                 annotation: mockAnnotation,
                 lastRefreshTime: mockLastRefreshTime,
                 data: [
-                  { 'Orders.count': 10, 'Orders.status': 'completed' },
+                  { 'Orders.count': 10, 'Orders.totalAmount': 500, 'Orders.status': 'completed' },
+                  { 'Orders.count': 5, 'Orders.totalAmount': 200, 'Orders.status': 'pending' },
                 ],
               });
             },
@@ -318,7 +327,7 @@ describe('GraphQL Schema', () => {
     test('should return annotation and lastRefreshTime in extensions', async () => {
       const query = `query CubeQuery {
         cube {
-          orders { count status }
+          orders { count totalAmount status }
         }
       }`;
 
@@ -332,6 +341,26 @@ describe('GraphQL Schema', () => {
       expect(res.body.extensions).toBeDefined();
 
       expect(res.body).toMatchSnapshot();
+    });
+
+    it('should accumulate all measures and dimensions', async () => {
+      const query = `query CubeQuery {
+        cube {
+          orders { count totalAmount status }
+        }
+      }`;
+
+      const res = await request(app)
+        .post('/graphql')
+        .set('Content-Type', 'application/json')
+        .send(gqlQuery(query));
+
+      expect(res.body.errors).toBeUndefined();
+
+      expect(res.body.data.cube).toEqual([
+        { orders: { count: '10', totalAmount: '500', status: 'completed' } },
+        { orders: { count: '5', totalAmount: '200', status: 'pending' } },
+      ]);
     });
   });
 });