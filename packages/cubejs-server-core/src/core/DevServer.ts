cc929464fdf28c521f63ee9b9de55d1b3fb59261
haechangcho <hc.cho@idk2.co.kr>
2026-05-13 19:52:13 +0900

    Serve playground directly at root

--- packages/cubejs-server-core/src/core/DevServer.ts
+++ packages/cubejs-server-core/src/core/DevServer.ts
@@ -73,10 +73,6 @@ export class DevServer {
     this.cubejsServer.event('Dev Server Start');
     const serveStatic = require('serve-static');
 
-    app.get('/', (req, res) => {
-      res.redirect(302, '/playground/');
-    });
-
     const catchErrors = (handler) => async (req, res, next) => {
       try {
         await handler(req, res, next);
